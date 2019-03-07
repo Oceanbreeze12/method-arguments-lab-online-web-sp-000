f488059a36b33a181d45d843f1e40ed2aabb01e5
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-07 23:26:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
-
-
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
 def introduction_with_launguage(name,launguage)
   puts "Hi,my name is #{name}and I am learning to program in #{launguage}."
